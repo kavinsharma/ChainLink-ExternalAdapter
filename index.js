b18d0859bf1f8dd0b014ad259616ab8d0e67faf0
Aryan <aryan@techosto.com>
2022-09-12 14:07:41 +0530

    integrated Finalytics

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Requester, Validator } = require('@chainlink/external-adapter')
+const { Requester, Validator } = require('@chainlink/external-adapter');
 
 
 const customError = (data) => {
@@ -7,8 +7,14 @@ const customError = (data) => {
 };
 
 const customParams = {
-  city: ['city', 'q', 'town'],
-  endpoint: true
+  start_date: true,
+  end_date: true,
+  endpoint: true,
+  sort: false,
+  putcountpercent_gte: false,
+  limit: false,
+  key: true,
+  subKey: true,
 };
 
 exports.createRequest = async (input, callback) => {
@@ -16,16 +22,46 @@ exports.createRequest = async (input, callback) => {
   const jobRunID = validator.validated.id
 
   try{
-    const endpoint = validator.validated.data.endpoint || 'weather';
-    const q = validator.validated.data.city;
-    const appId = process.env.API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/${endpoint}?q=${q}&appid=${appId}`;
+    const endpoint = validator.validated.data.endpoint;
+    const start_date = validator.validated.data.start_date;
+    const end_date = validator.validated.data.end_date;
+    const limit = validator.validated.data.limit;
+    const sort = validator.validated.data.sort;
+    const putcountpercent_gte = validator.validated.data.putcountpercent_gte;
+    const key = validator.validated.data.key;
+    const subKey = validator.validated.data.subKey;
+
+    const url = `https://www.finlytica.com/${endpoint}?`;
+
+    const params = {
+      start_date,
+      end_date
+    };
+
+    if(limit) params.limit = limit;
+    if(sort) params.limit = sort;
+    if(putcountpercent_gte) params.limit = putcountpercent_gte;
+
+    const tokenConfig = {
+      method: 'post',
+      url: 'https://www.finlytica.com/auth/local',
+      data: {
+        identifier: process.env.identifier,
+        password: process.env.password,
+      }
+    };
+    const token = await Requester.request(tokenConfig, customError);
+
     const config = {
-      url
+      url,
+      params,
+      headers: {
+        Authorization: `Bearer ${token.data.jwt}`
+      }
     };
 
     const response = await Requester.request(config, customError);
-    response.data.result = Requester.validateResultNumber(response.data, ['main', 'temp']);
+    response.data.result = Requester.validateResultNumber(response.data, [key, subKey]);
 
     callback(response.status, Requester.success(jobRunID, response));
   } catch (err) {
